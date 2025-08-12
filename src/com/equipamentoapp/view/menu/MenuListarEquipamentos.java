9a9a6df61feecb41bb39c08038c75da9422b1852
Luca5Eckert <ecklukatv@gmail.com>
2025-08-11 21:50:20 -0300

    feat: adiciona chamar menu listar

--- src/com/equipamentoapp/view/menu/MenuListarEquipamentos.java
+++ src/com/equipamentoapp/view/menu/MenuListarEquipamentos.java
@@ -1,5 +1,6 @@
 package com.equipamentoapp.view.menu;
 
+import com.equipamentoapp.model.enums.TipoEquipamento;
 import com.equipamentoapp.view.Leitor;
 
 public class MenuListarEquipamentos extends Menu<String>{
@@ -10,6 +11,19 @@ public class MenuListarEquipamentos extends Menu<String>{
         System.out.println("==========================================");
         System.out.println("            LISTAR EQUIPAMENTOS           ");
         System.out.println("==========================================");
+        TipoEquipamento[] tipoEquipamentos = selecionarTipoEspecifico(leitor);
+    }
+
+    private TipoEquipamento[] selecionarTipoEspecifico(Leitor leitor) {
+        System.out.println(" T- Todos os tipos ");
+        System.out.println(" Ou selecionar tipo especifico:");
+        TipoEquipamento.listarTodos();
+        String opcao = leitor.lerLinha();
+
+        return switch(opcao){
+            case "T" -> TipoEquipamento.values();
+            default -> new TipoEquipamento[]{TipoEquipamento.values()[Integer.parseInt(opcao)]};
+        };
     }
 
     @Override

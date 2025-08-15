9f7e3c93ab3121dc2cc4b31093f96ef7b3f39af6
Luca5Eckert <ecklukatv@gmail.com>
2025-08-15 16:07:05 -0300

    feat: adiciona MenuListarEquipamentosPorNome

--- src/com/equipamentoapp/view/menu/MenuListarEquipamentosPorNome.java
+++ src/com/equipamentoapp/view/menu/MenuListarEquipamentosPorNome.java
@@ -1,4 +1,63 @@
 package com.equipamentoapp.view.menu;
 
-public class MenuListarEquipamentosPorNome {
+import com.equipamentoapp.controller.EstoqueController;
+import com.equipamentoapp.dto.EquipamentoResponse;
+import com.equipamentoapp.view.Leitor;
+
+import java.util.List;
+
+public class MenuListarEquipamentosPorNome extends Menu<String> {
+
+    private final EstoqueController estoqueController;
+    private Menu<?> proximoMenu;
+
+    public MenuListarEquipamentosPorNome(EstoqueController estoqueController) {
+        this.estoqueController = estoqueController;
+    }
+
+    @Override
+    public void chamarMenu(Leitor leitor) {
+        System.out.println("==========================================");
+        System.out.println("            LISTAR EQUIPAMENTOS           ");
+        System.out.println("==========================================");
+        System.out.println(" Nome desejado: ");
+        String nomeDesejado = leitor.lerLinha();
+
+        List<EquipamentoResponse> listaEquipamentos = estoqueController.listarEquipamentosPorNome(nomeDesejado);
+
+        listarEquipamentos(listaEquipamentos);
+
+        System.out.println("------------------------------------------");
+
+        System.out.println(" S- Sair");
+        System.out.println(" T- Tenter outro Nome");
+        System.out.println("==========================================");
+
+        setResposta(leitor.lerLinha());
+
+
+    }
+
+    private void listarEquipamentos(List<EquipamentoResponse> listaEquipamentos) {
+        System.out.println(" EQUIPAMENTOS: ");
+        if(listaEquipamentos.isEmpty()){
+            System.out.println(" Estoque Vazio ");
+            return;
+        }
+        listaEquipamentos.forEach(System.out::println);
+    }
+
+    @Override
+    public void executarAcao() {
+        proximoMenu = switch (getResposta()){
+            case "S" -> new MenuInicial();
+            case "T" -> this;
+            default -> proximoMenu;
+        };
+    }
+
+    @Override
+    public Menu<?> proximoMenu() {
+        return proximoMenu;
+    }
 }
